9bb3b3318f307c4893f229cc719625992aa769dc
Yunkai Yang <yunkai_yang@sina.com>
2025-12-30 13:34:33 +0800

    update README.md

--- train.sh
+++ train.sh
@@ -1,7 +1,9 @@
-python train.py \
-    --pretrained_model_name_or_path sd3.5_medium \
-    --data_root demo \
-    --work_dir demo \
-    --train_file emo/index_.jsonl \
-    --vectors_path vectors/demo \
-    --num_cls 18
+ngpu=4 accelerate launch --config_file config/default.yaml \
+    --num_processes=${ngpu} \
+    train.py \
+        --pretrained_model_name_or_path sd3.5_medium \
+        --data_root demo \
+        --work_dir demo \
+        --train_file emo/index_.jsonl \
+        --vectors_path vectors/demo \
+        --num_cls 18
